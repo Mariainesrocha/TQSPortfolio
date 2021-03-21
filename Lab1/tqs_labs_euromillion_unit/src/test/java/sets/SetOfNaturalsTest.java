db585f5cd5d05c3b1d1f3fd6cc28709e287a41be
Mariainesrocha <ines.seabrarocha@ua.pt>
2021-03-21 20:27:38 +0000

    ex2_euromillions

---
+++ Lab1/tqs_labs_euromillion_unit/src/test/java/sets/SetOfNaturalsTest.java
@@ -0,0 +1,107 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * @author ico0
+ */
+public class SetOfNaturalsTest {
+    private SetOfNaturals setA;
+    private SetOfNaturals setB;
+    private SetOfNaturals setC;
+    private SetOfNaturals setD;
+
+    @BeforeEach
+    public void setUp() {
+        setA = new SetOfNaturals();
+        setB = SetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
+
+        setC = new SetOfNaturals();
+        for (int i = 5; i < 50; i++) {
+            setC.add(i * 10);
+        }
+        setD = SetOfNaturals.fromArray(new int[]{30, 40, 50, 60, 10, 20});
+    }
+
+    @AfterEach
+    public void tearDown() {
+        setA = setB = setC = setD = null;
+    }
+
+    @Test
+    public void testAddElement() {
+
+        setA.add(99);
+        assertTrue(setA.contains(99), "add: added element not found in set.");
+        assertEquals(1, setA.size());
+
+        setB.add(11);
+        assertTrue(setB.contains(11), "add: added element not found in set.");
+        assertEquals(7, setB.size(), "add: elements count not as expected.");
+    }
+
+    @Test
+    public void testAddBadArray() {
+        int[] elems = new int[]{10, 20, -30};
+
+        // must fail with exception
+        assertThrows(IllegalArgumentException.class, () -> setA.add(elems));
+    }
+
+
+    @Test
+    public void testIntersectForNoIntersection() {
+        assertFalse(setA.intersects(setB), "no intersection but was reported as existing");
+        assertTrue(setC.intersects(setB), "intersection not reported as it was supposed to");
+    }
+
+    //------------ 2b
+    @Test
+    public void testAddNotNaturalEl(){
+
+        // must fail with exception
+        assertThrows(IllegalArgumentException.class,() -> {setA.add(-1);});
+    }
+
+    @Test
+    public void testAddDuplicatedValue(){
+
+        // must fail with exception
+        assertThrows(IllegalArgumentException.class,() -> {setD.add(60);});
+    }
+
+    @Test
+    public void testSetSize() {
+        assertEquals(0, setA.size(),"size: empty array but wrong set size returned");
+    }
+
+    @Test
+    public void testContains() {
+        assertTrue(setD.contains(30),"contains: element not found in set");
+        assertFalse(setD.contains(1),"contains: element found in set when not supposed to");
+    }
+
+    @Test
+    public void testAddDuplicateArray() {
+        int[] elemArray = {2,50,2};
+        assertThrows(IllegalArgumentException.class, () -> setA.add(elemArray));
+
+        int[] elemArrayB = {40,60,2};
+        assertThrows(IllegalArgumentException.class, () -> setB.add(elemArrayB));
+    }
+
+    //Check that you if you create a set from an array, if that array has duplicates an IllegalArgumentException is raised
+    @Test
+    public void testNewSetFromArray() {
+        int[] elemArray = {1,2,3,1};
+        assertThrows(IllegalArgumentException.class, () -> setA.fromArray(elemArray));
+    }
+
+
+}
