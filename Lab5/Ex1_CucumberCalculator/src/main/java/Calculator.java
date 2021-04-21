cae94b4c9d106a32e126686bd2e79a75d41510ed
Mariainesrocha <ines.seabrarocha@ua.pt>
2021-04-21 16:47:33 +0100

    ex1 done

---
+++ Lab5/Ex1_CucumberCalculator/src/main/java/Calculator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Calculator {
+    private final Deque<Number> stack = new LinkedList<Number>();
+    private static final List<String> OPS = Arrays.asList("-", "+", "*", "/");
+
+    public void push(Object arg) {
+        if (OPS.contains(arg)) {
+            Number y = stack.removeLast();
+            Number x = stack.isEmpty() ? 0 : stack.removeLast();
+            Double val = null;
+            if (arg.equals("-")) {
+                val = x.doubleValue() - y.doubleValue();
+            } else if (arg.equals("+")) {
+                val = x.doubleValue() + y.doubleValue();
+            } else if (arg.equals("*")) {
+                val = x.doubleValue() * y.doubleValue();
+            } else if (arg.equals("/")) {
+                val = x.doubleValue() / y.doubleValue();
+            }
+            push(val);
+        } else {
+            stack.add((Number) arg);
+        }
+    }
+
+    public Number value() {
+        return stack.getLast();
+    }
+}
